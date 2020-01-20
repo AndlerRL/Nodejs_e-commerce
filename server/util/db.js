1d67aa15a04d3e66f15b003a4dc2623f151afb93
Meynor R Lucas <andre.rlucas@outlook.com>
2020-01-20 09:30:23 -0600

    settup change

--- server/util/db.js
+++ server/util/db.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
 const mysql2 = require('mysql2');
 
-const sequelize = new Sequelize(`nodejs_ecommerce`, `root`, `Meynorromero-94.`, {
+const sequelize = new Sequelize(process.env.NODE_ENV === 'production' ? 'shop4any' : `nodejs_ecommerce`, `root`, `Meynorromero-94.`, {
   dialect: 'mysql',
   dialectModule: mysql2,
-  host: process.env.NODE_ENV === 'production' ? 'https://nodejs-ecommerce.netlify.com/.netlify/functions/server/' : 'localhost'
+  host: process.env.NODE_ENV === 'production' ? 'shop4any.herokuapp.com' : 'localhost'
 });
 
 module.exports = sequelize;
